8aecf9fa16aaf20acbca3551318a936312a1c73a
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-06-21 19:21:08 -0500

    chore: add routes behaviour

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+//import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //{ path: '',                 redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '**',               component: PageNotFoundComponent }
+];
