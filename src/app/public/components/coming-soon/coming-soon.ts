dfbac5fd9396e691f4404db698b62c9e98db6341
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-07-05 10:39:23 -0500

    feat: add place holder component for non implemented features

--- src/app/public/components/coming-soon/coming-soon.ts
+++ src/app/public/components/coming-soon/coming-soon.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-coming-soon',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    TranslateModule
+  ],
   templateUrl: './coming-soon.html',
-  styleUrl: './coming-soon.css'
+  styleUrl: 'coming-soon.css',
 })
 export class ComingSoon {
-
+  @Input() messagePath!: string;
 }
