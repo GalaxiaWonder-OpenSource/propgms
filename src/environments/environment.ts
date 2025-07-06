97acd6bd75e0bd4d8377f5efd13b38f309d3fc77
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-07-06 05:33:17 -0500

    hotfix: replace localhost url with deployed url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'http://localhost:8080/api/v1',
+  serverBaseUrl: 'http://172.179.245.34:8080/api/v1',
   authorizationPath: '/auth',
   organizationPath: '/organizations',
   invitationPath: '/organizations/invitations',
